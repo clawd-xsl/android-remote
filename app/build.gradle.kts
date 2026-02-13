d6f37d5ae8198de2cd1aa5aed171fc27141233dd
小C (clawd-xsl) <clawd.xsl@gmail.com>
2026-02-12 18:58:03 -0800

    fix: use MaterialComponents theme + add material dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation("org.nanohttpd:nanohttpd:2.3.1")
     implementation("com.google.code.gson:gson:2.10.1")
